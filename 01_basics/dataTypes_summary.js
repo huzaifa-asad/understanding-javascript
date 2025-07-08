ff3b43698638cc5290fac7b5b73f33eaeea6ccbb
huzaifazz <khuzaifaasad55@gmail.com>
2025-07-08 05:39:29 -0700

    add stack and heap concept

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -30,3 +30,39 @@ console.log(id.toString()); // Symbol(1234)
 let bigIntValue = 45678901234567890n; // using 'n' at the end to denote BigInt
 console.log(typeof bigIntValue); // bigint
 console.log(bigIntValue); // 45678901234567890n
+
+
+//--------------------------------------------------------Memory------------------------------------------------------------
+
+// Memory in JavaScript is managed automatically by the JavaScript engine, which uses a garbage collector to reclaim memory that is no longer needed.
+// When a variable is created, memory is allocated for it. When the variable goes out of scope or is no longer referenced, the garbage collector will free up that memory.
+// JavaScript uses a heap for dynamic memory allocation, which allows for the creation of complex data structures like objects and arrays.
+// The memory model in JavaScript is based on the concept of "references" for non-primitive data types.
+// Primitive data types are stored directly in the variable, while non-primitive data types are stored as references to their location in memory.
+// This means that when you assign an object or array to a new variable, you are copying the reference, not the actual data.
+// This can lead to unexpected behavior when modifying objects or arrays, as changes made to one reference will affect all references to that object or array.
+
+// Stack (Primitive data types), Heap (Non-Primitive data types)
+
+let myGithubName = "huzaifazz";
+let anotherGithubName = myGithubName; // primitive data type, copied value
+anotherGithubName = "huzaifa asad";
+
+console.log(myGithubName);
+console.log(anotherGithubName);
+
+// Heap (Non-Primitive data types):
+let myGithubObject = {
+  name: "HUZAIFA ASAD",
+  age: 20
+};
+
+let anotherGithubObject = myGithubObject; // non-primitive data type, copied reference
+console.log(myGithubObject);
+console.log(anotherGithubObject);
+
+console.log("After Modifying:");
+
+anotherGithubObject.name = "Web developer"; // modifying the object through another reference
+console.log(myGithubObject);
+console.log(anotherGithubObject);
