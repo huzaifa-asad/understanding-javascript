6aa78a24de44f53aae362f9b482840cc6653f2d9
Huzaifazz <khuzaifaasad55@gmail.com>
2025-07-15 12:14:23 -0700

    functions in detail with object and array as parameters

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,3 +32,27 @@ function multiplyNumbers(num1 = 1, num2 = 2) {
 }
 console.log(multiplyNumbers()); // Using default parameters
 console.log(multiplyNumbers(5, 10));
+
+// Rest Operators
+function calculateCartPrice(...num1) {
+    return num1
+}
+console.log(calculateCartPrice(342, 532, 543, 643));
+
+// objects as function parameters
+const user = {
+    username: "huzaifa",
+    age: 20
+}
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and age is ${anyobject.age}`);
+}
+handleObject(user);
+
+// Array as function parameters
+const myArr = [243, 563, 654, 865, 455]
+
+function handleArray(getarray) {
+    return getarray
+}
+console.log(handleArray(myArr))
